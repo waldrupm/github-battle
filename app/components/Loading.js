c46d3972c26fb1fe8c62cfb68b8305e4ab40e957
Micheal Waldrup <waldrupm@gmail.com>
2019-05-07 10:42:53 -0400

    Refactor all to use babel transform class properties

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -9,12 +9,19 @@ const styles = {
 };
 
 class Loading extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      text: props.text
-    }
+  state = {
+    text: this.props.text
+  }
+
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    speed: PropTypes.number.isRequired
+  }
+
+  static defaultProps = {
+    text: 'Loading',
+    speed: 300
   }
 
   componentDidMount() {
@@ -37,16 +44,6 @@ class Loading extends React.Component {
   componentWillUnmount() {
     window.clearInterval(this.interval);
   }
-}
-
-Loading.propTypes = {
-  text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired
-}
-
-Loading.defaultProps = {
-  text: 'Loading',
-  speed: 300
 }
 
 export default Loading;
